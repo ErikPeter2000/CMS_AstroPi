438b855c8fa2ec7e6d4abf5ee79df8e7fb8a7284
ErikPeter2000 <erik.hurinek@gmx.co.uk>
2024-01-19 08:09:23 +0000

    matching now done in background thread

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from speedWorker import speedWorker as Worker # to be replaced with a reference to teh calculation Team's worker
 from dummyCameraWrapper import CameraWrapper # replace with actual camera wrapper
-from cv2Matcher import MatchData, calculateMatches
+from cv2Matcher import ImagePair, imageToCv2, timeDifference
 from sensorDumpWrapper import SensorDumpWrapper
 import threading
 from datetime import datetime
@@ -14,6 +14,7 @@ ROOT_FOLDER = (Path(__file__).parent).resolve()
 DATA_FOLDER = ROOT_FOLDER
 MAX_CALC_TIME = 10 # seconds
 INTERVAL = 1 # seconds
+GSD = 1.8 # km/pixel
 
 def writeSpeed(speed):
     """Writes the speed to speed.txt."""
@@ -26,14 +27,14 @@ if __name__ == "__main__":
     startTime = datetime.now()
 
     logger.info(f"Starting program")
-    speedWorker = Worker() # to be replaced with the calculation team's worker
+    speedWorker = Worker(GSD) # to be replaced with the calculation team's worker
     speedThread = threading.Thread(target=speedWorker.work) # initialise the worker thread
-    speedThread.start() # start the worker thread
 
     sensorDumpWrapper = SensorDumpWrapper(DATA_FOLDER) # initialise the sensor dump wrapper
     cameraWrapper = CameraWrapper()
     imageQueue = Queue() # store the last two images needed to compute matches
     try:
+        speedThread.start() # start the worker thread
         imageIndex = 0
         while (datetime.now() - startTime).total_seconds() < MAX_CALC_TIME: # run for MAX_CALC_TIME seconds
             currentImagePath = DATA_FOLDER / Path(f"./image{imageIndex}.jpg") # the path to store the captured image
@@ -43,8 +44,11 @@ if __name__ == "__main__":
             if (imageQueue.qsize() == 2): # if there are two images to match...
                 image1 = imageQueue.get() # dequeue the oldest image
                 image2 = imageQueue.queue[0] # peek at the newer image
-                matchData = calculateMatches(image1, image2)
-                logger.info(f"Matches: {len(matchData)} Time Difference: {matchData.timeDifference}")
+                image1cv2 = imageToCv2(image1) # convert the images to cv2 images
+                image2cv2 = imageToCv2(image2)
+                timeDiff = timeDifference(image1, image2) # calculate the time difference between the images
+                matchData = ImagePair(image1cv2, image2cv2, timeDiff)
+                logger.info(f"Captured Image, Time Difference: {matchData.timeDifference}")
                 os.remove(image1) # delete the oldest image; we dont need it any more
 
                 speedWorker.queue.put(matchData) # enqueue the match data for the speed worker
--- src/speedWorker.py
+++ src/speedWorker.py
@@ -2,41 +2,39 @@ import math
 
 from worker import Worker
 from logzero import logger
+from cv2Matcher import ImagePair, calculateMatches
 
-
-
-
-#__________________________________!!!!!!!!!!!!!!!!!!!__________________________
-#To be worked on tomorrow
-def calculate(match_data):
-    c1 = match_data.coordinates_1
-    c2 = match_data.coordinates_2
-    td = match_data.timeDifference
-    av_d = 0
-    for i in range(0, len(c1)):
-        x1 = c1[i][0]
-        y1 = c1[i][1]
-        x2 = c2[i][0]
-        y2 = c2[i][1]
-        d = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-        av_d = (av_d*i + d)/(i+1)
-    return av_d/td
-
-#__________________________________!!!!!!!!!!!!!!!!!!!__________________________
 class speedWorker(Worker):
-    def __init__(self):
+    def __init__(self, gsd):
         super().__init__()
+        self.gsd = gsd
         self._Worker__value = 0
+
+    def calculate(self, match_data, gsd):
+        c1 = match_data.coordinates_1
+        c2 = match_data.coordinates_2
+        td = match_data.timeDifference
+        av_d = 0
+        for i in range(0, len(c1)):
+            x1 = c1[i][0]
+            y1 = c1[i][1]
+            x2 = c2[i][0]
+            y2 = c2[i][1]
+            d = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+            av_d = (av_d*i + d)/(i+1)
+        return av_d/td*gsd
 
     def work(self):
         count = 0  # local variables for number of items in the queue
         while not self.cancelled:  # loop until cancelled
             try:
                 if not self.queue.empty():
-                    item = self.queue.get(False)
+                    imagePair = self.queue.get(False)
+                    matchData = calculateMatches(imagePair)
+
                     count+=1
                     speed = self._Worker__value
-                    speed = (speed*count + calculate(item))/(count+1)
+                    speed = (speed*count + self.calculate(matchData, self.gsd))/(count+1)
                     self._Worker__value = speed    
             except Exception as e:
                 logger.error(e)
