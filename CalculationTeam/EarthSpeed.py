c2aa63061aa20ea56baa224bd3ab8eb01a4958f6
YotamLW <18ylimor-waisberg@cms.tela.org.uk>
2024-02-12 21:21:05 +0000

    mathematical error fix

--- CalculationTeam/EarthSpeed.py
+++ CalculationTeam/EarthSpeed.py
@@ -9,7 +9,9 @@ matchSpeed = 0 #metres per second
 #the variable currentInclination should be set to the ISS's current latitude in degrees
 #the variable matchSpeed should be set to the aquired speed value from the matching points
 
-def calculate(radius, spinTime, maxInclination, currentInclination, matchSpeed):
+def removeEarthVelocity(radius, spinTime, maxInclination, currentInclination, matchSpeed):
+    if currentInclination > 51.7: currentInclination = 51.6
+    if currentInclination < -51.7: currentInclination = -51.6
     newRadius = radius * math.cos(math.radians(currentInclination)) #radius of Earth relative to the ISS parallel to the equator
     newCircumference = math.pi * (newRadius ** 2) #circumference of Earth at this point
     newEarthSpeed = newCircumference / spinTime #speed of Earth at this point
@@ -20,5 +22,5 @@ def calculate(radius, spinTime, maxInclination, currentInclination, matchSpeed):
     if aC != 0:
         newSpeed = (c * math.sin(math.pi - aC - math.asin((a * math.sin(aC)) / c))) / (math.sin(aC)) #velocity triangle and trigonometry
     else:
-        newSpeed = a + c #in case aC = 0
+        newSpeed = c - a #in case aC = 0
     return newSpeed #returns the adjusted speed in metres per second
